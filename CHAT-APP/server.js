0dc7c0762261327058e812b297893f0755cf0a89
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-11-22 14:14:51 +0530

    User identification

--- CHAT-APP/server.js
+++ CHAT-APP/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import {Server} from 'socket.io';
-import cors from 'cors';
-import http from 'http';
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+import http from "http";
 
 const app = express();
 
@@ -9,32 +9,38 @@ const app = express();
 
 const server = http.createServer(app);
 
-
 //2. Create socket server
 
 const io = new Server(server, {
-    cors:{
-        origin:'*',
-        methods:['GET', 'POST']
-    }
-})
-
-
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 // 3. Use socket event
 
-io.on('connection', (socket)=> {
-    console.log('Connection is establise');
-    socket.on('new_message', (message)=> {
-        socket.broadcast.emit('broadcast_message',message);
-    })
-    socket.on('disconnect', ()=> {
-        console.log('Connection is disconnect');
-    })
-})
+io.on("connection", (socket) => {
+  console.log("Connection is establise");
 
+  socket.on("join", (username) => {
+    socket.username = username;
+  })
 
+    socket.on("new_message", (message) => {
+      // broadcasting this message to all the clients
+      let userMessage = {
+        username: socket.username,
+        message: message,
+      };
+      socket.broadcast.emit("broadcast_message", userMessage);
+    });
+  
+  socket.on("disconnect", () => {
+    console.log("Connection is disconnect");
+  });
+});
 
-server.listen(3000, ()=> {
-    console.log('App is listening on port 3000');
-})
+server.listen(3000, () => {
+  console.log("App is listening on port 3000");
+});
